b102e52f37494b6de53e602e4ba08e419a951202
unknown <nathalia.mota@etec.sp.gov.br>
2023-03-31 21:46:13 -0300

    added infotempo content

--- PrevisaoTempo/InfoTempo.cs
+++ PrevisaoTempo/InfoTempo.cs
@@ -6,5 +6,36 @@ using System.Threading.Tasks;
 
 namespace PrevisaoTempo
 {
+    class InfoTempo
+    {
+        public class coordenada
+        {
+            double latitude { get; set; }
+            double longitude { get; set; }
+        }
 
+        public class tempo
+        {
+            string main { get; set; }
+            string descricao { get; set; }
+            string icone { get; set; }
+        }
+
+        public class main
+        {
+            double temperatura { get; set; }
+            double sensaTermica { get; set; }
+            double tempMin { get; set; }
+            double tempMax { get; set; }
+            double humidade { get; set; }
+
+        }
+        
+        public class root
+        {
+            public coordenada coordenada { get; set; }
+            public List <tempo> tempo { get; set; }
+            public main main { get; set; }
+        }
+    }
 }
