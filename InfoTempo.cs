1ccacacb8cfebb7d65b9f683416add552e3ae440
Nathalia Monalisa <119463033+lialisa55@users.noreply.github.com>
2023-04-04 22:56:13 -0300

    fim

---
+++ InfoTempo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevisaoTempo
+{
+    class InfoTempo
+    {
+
+        //configuração, obtenção das informações da API
+        public class coord
+        {
+            double latitude { get; set; }
+            double longitude { get; set; }
+        }
+
+        public class weather
+        {
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class main
+        {
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+
+        }
+        
+        public class root
+        {
+            public coord coord { get; set; }
+            public List <weather> weather { get; set; }
+            public main main { get; set; }
+        }
+    }
+}
