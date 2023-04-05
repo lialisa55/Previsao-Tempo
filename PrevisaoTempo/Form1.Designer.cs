fc31a09e29935a93e57ec2a3e75bdea10f9d8ad4
Gabriel Ribeiro <118030785+gabivirus@users.noreply.github.com>
2023-04-04 21:09:39 -0300

    Add files via upload

--- PrevisaoTempo/Form1.Designer.cs
+++ PrevisaoTempo/Form1.Designer.cs
@@ -29,38 +29,39 @@ namespace PrevisaoTempo
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtCidade = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pbTempoIcone = new System.Windows.Forms.PictureBox();
             this.btnProcurarCidade = new System.Windows.Forms.Button();
-            this.lbCondicao = new System.Windows.Forms.Label();
             this.lbDescricao = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.lbTemperatura = new System.Windows.Forms.Label();
             this.lbSensacaoTermica = new System.Windows.Forms.Label();
-            this.lbTemperaturaMax = new System.Windows.Forms.Label();
-            this.lbTemperaturaMin = new System.Windows.Forms.Label();
+            this.lbTemperaturaMinMax = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pbTempoIcone)).BeginInit();
             this.SuspendLayout();
             // 
             // txtCidade
             // 
             resources.ApplyResources(this.txtCidade, "txtCidade");
+            this.txtCidade.BackColor = System.Drawing.SystemColors.HighlightText;
             this.txtCidade.Name = "txtCidade";
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
             this.label1.Name = "label1";
             // 
             // pbTempoIcone
             // 
             resources.ApplyResources(this.pbTempoIcone, "pbTempoIcone");
+            this.pbTempoIcone.BackColor = System.Drawing.Color.Transparent;
             this.pbTempoIcone.Name = "pbTempoIcone";
             this.pbTempoIcone.TabStop = false;
             // 
@@ -68,91 +69,77 @@ namespace PrevisaoTempo
             // 
             resources.ApplyResources(this.btnProcurarCidade, "btnProcurarCidade");
             this.btnProcurarCidade.BackColor = System.Drawing.Color.Transparent;
+            this.btnProcurarCidade.ForeColor = System.Drawing.SystemColors.Control;
             this.btnProcurarCidade.Name = "btnProcurarCidade";
             this.btnProcurarCidade.UseVisualStyleBackColor = false;
             this.btnProcurarCidade.Click += new System.EventHandler(this.btnProcurarCidade_Click);
-            // 
-            // lbCondicao
-            // 
-            resources.ApplyResources(this.lbCondicao, "lbCondicao");
-            this.lbCondicao.BackColor = System.Drawing.Color.Transparent;
-            this.lbCondicao.Name = "lbCondicao";
             // 
             // lbDescricao
             // 
             resources.ApplyResources(this.lbDescricao, "lbDescricao");
             this.lbDescricao.BackColor = System.Drawing.Color.Transparent;
+            this.lbDescricao.ForeColor = System.Drawing.SystemColors.Control;
             this.lbDescricao.Name = "lbDescricao";
-            // 
-            // label4
-            // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Name = "label4";
-            // 
-            // label5
-            // 
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Name = "label5";
-            // 
-            // label6
-            // 
-            resources.ApplyResources(this.label6, "label6");
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Name = "label6";
-            // 
-            // label7
-            // 
-            resources.ApplyResources(this.label7, "label7");
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Name = "label7";
             // 
             // lbTemperatura
             // 
             resources.ApplyResources(this.lbTemperatura, "lbTemperatura");
             this.lbTemperatura.BackColor = System.Drawing.Color.Transparent;
+            this.lbTemperatura.ForeColor = System.Drawing.SystemColors.Control;
             this.lbTemperatura.Name = "lbTemperatura";
             // 
             // lbSensacaoTermica
             // 
             resources.ApplyResources(this.lbSensacaoTermica, "lbSensacaoTermica");
             this.lbSensacaoTermica.BackColor = System.Drawing.Color.Transparent;
+            this.lbSensacaoTermica.ForeColor = System.Drawing.SystemColors.Control;
             this.lbSensacaoTermica.Name = "lbSensacaoTermica";
             // 
-            // lbTemperaturaMax
+            // lbTemperaturaMinMax
             // 
-            resources.ApplyResources(this.lbTemperaturaMax, "lbTemperaturaMax");
-            this.lbTemperaturaMax.BackColor = System.Drawing.Color.Transparent;
-            this.lbTemperaturaMax.Name = "lbTemperaturaMax";
+            resources.ApplyResources(this.lbTemperaturaMinMax, "lbTemperaturaMinMax");
+            this.lbTemperaturaMinMax.BackColor = System.Drawing.Color.Transparent;
+            this.lbTemperaturaMinMax.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbTemperaturaMinMax.Name = "lbTemperaturaMinMax";
             // 
-            // lbTemperaturaMin
+            // label2
             // 
-            resources.ApplyResources(this.lbTemperaturaMin, "lbTemperaturaMin");
-            this.lbTemperaturaMin.BackColor = System.Drawing.Color.Transparent;
-            this.lbTemperaturaMin.Name = "lbTemperaturaMin";
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Name = "label2";
+            // 
+            // statusStrip1
+            // 
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::PrevisaoTempo.Properties.Resources.dia;
-            this.Controls.Add(this.lbTemperaturaMin);
-            this.Controls.Add(this.lbTemperaturaMax);
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BackgroundImage = global::PrevisaoTempo.Properties.Resources.background;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTemperaturaMinMax);
             this.Controls.Add(this.lbSensacaoTermica);
             this.Controls.Add(this.lbTemperatura);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.lbDescricao);
-            this.Controls.Add(this.lbCondicao);
             this.Controls.Add(this.btnProcurarCidade);
             this.Controls.Add(this.pbTempoIcone);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCidade);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbTempoIcone)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -165,16 +152,13 @@ namespace PrevisaoTempo
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbTempoIcone;
         private System.Windows.Forms.Button btnProcurarCidade;
-        private System.Windows.Forms.Label lbCondicao;
         private System.Windows.Forms.Label lbDescricao;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label lbTemperatura;
         private System.Windows.Forms.Label lbSensacaoTermica;
-        private System.Windows.Forms.Label lbTemperaturaMax;
-        private System.Windows.Forms.Label lbTemperaturaMin;
+        private System.Windows.Forms.Label lbTemperaturaMinMax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PrevisaoTempo/Form1.cs
+++ PrevisaoTempo/Form1.cs
@@ -19,6 +19,11 @@ namespace PrevisaoTempo
             InitializeComponent();
         }
 
+        public double ToCelsius(double kelvin)
+        {
+            return Convert.ToInt32(kelvin - 273.15);
+        }
+
         string OpenWeatherKey = "d3a690b128e500aa56dded73551bda8c";
 
         void GetPrevisao()
@@ -29,13 +34,13 @@ namespace PrevisaoTempo
                 var json = web.DownloadString(url);
                 InfoTempo.root Info = JsonConvert.DeserializeObject<InfoTempo.root>(json);
 
-                pbTempoIcone.ImageLocation = "https://openweathermap.org/img/w" + Info.weather[0].icon + ".png";
-                lbCondicao.Text = Info.weather[0].main;
+                pbTempoIcone.ImageLocation = "https://openweathermap.org/img/wn/" + Info.weather[0].icon + "@2x.png";
                 lbDescricao.Text = Info.weather[0].description;
-                lbSensacaoTermica.Text = Info.main.feels_like.ToString();
-                lbTemperatura.Text = Info.main.temp.ToString();
-                lbTemperaturaMax.Text = Info.main.temp_max.ToString();
-                lbTemperaturaMin.Text = Info.main.temp_min.ToString();
+                lbSensacaoTermica.Text = ToCelsius(Info.main.feels_like).ToString() + '°';
+                lbTemperatura.Text = ToCelsius(Info.main.temp).ToString() + '°';
+                lbTemperaturaMinMax.Text = ToCelsius(Info.main.temp_min).ToString() + '°' + '/' + ToCelsius(Info.main.temp_max).ToString() + '°';
+                label2.Visible = true;
+                lbDescricao.Visible = true;
             }
         }
 
@@ -49,6 +54,21 @@ namespace PrevisaoTempo
             {
                 MessageBox.Show("Cidade inválida");
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            statusStrip1.Text = DateTime.Today.ToLongDateString();
         }
     }
 }
